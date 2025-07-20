b9dca0d0d7b20bafad3d6b2dac236cc77cd03c6a
Abin970 <ajmandapam@gmail.com>
2025-07-20 12:56:00 +0530

    Initial commit

---
+++ src/test/java/dev/scaler/abin/e_comuser_service/EComUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.scaler.abin.e_comuser_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EComUserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
