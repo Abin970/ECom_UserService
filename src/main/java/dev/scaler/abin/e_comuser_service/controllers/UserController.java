d4ee7ca68d1bd45e7593ee4d67a4a4a494ac44c8
Abin970 <ajmandapam@gmail.com>
2025-08-04 20:33:54 +0530

    user_login & authentication

---
+++ src/main/java/dev/scaler/abin/e_comuser_service/controllers/UserController.java
@@ -0,0 +1,4 @@
+package dev.scaler.abin.e_comuser_service.controllers;
+
+public class UserController {
+}
---
+++ src/main/java/dev/scaler/abin/e_comuser_service/services/IRoleService.java
@@ -0,0 +1,4 @@
+package dev.scaler.abin.e_comuser_service.services;
+
+public interface IRoleService {
+}
